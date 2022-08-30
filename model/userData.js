0f52da10b1ee088fa228017a3add7dff00337031
Ashik cv <101665093+Ashikcv11@users.noreply.github.com>
2022-08-30 12:55:39 +0530

    Update userData.js
    final commit

--- model/userData.js
+++ model/userData.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-// const DB = "mongodb+srv://sajithjayaram:mylibraryapp@cluster0.2pltx.mongodb.net/librarydb?retryWrites=true&w=majority"
+
 const DB = "mongodb+srv://ashikcv:NtZ7fORecmRAHuul@cluster0.xwu725d.mongodb.net/?retryWrites=true&w=majority"
 
 
@@ -9,17 +9,6 @@ mongoose.connect(DB,{ useNewUrlParser: true, useUnifiedTopology: true}).then(()=
  console.log(err)
 })
 
-// const userSchema = new mongoose.Schema({
-//     email: { type: String,required:true, unique: true},
-//     otp: {type: String, required:false},
-// });
-
-// var userSchema = new mongoose.Schema({
-//     email: String,
-//     otp:Number
-//    }, {
-//     versionKey: false
-//    })
 
 const Schema = mongoose.Schema;
 
